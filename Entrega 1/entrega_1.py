89a8ab23a27f9aa5587aa6ff533f3ff908be0b61
Juan Cruz <juancruzroussilian9@gmail.com>
2022-09-08 15:58:49 -0300

    Agrego pseudocodigo y archivo txt

---
+++ Entrega 1/entrega_1.py
@@ -0,0 +1,19 @@
+clothes_file = open("primer_problema.txt")
+laundry_file = open("resultado.txt","x")
+
+for line in clothes_file:
+    if "primer caracter es e ":
+        #Agrego incompatibilidad al hashmap
+    elif "primer caracter es n":
+        #Agrego prenda a lista de prendas
+
+#creo lista de listas de prendas
+
+for prenda in prendas:
+    # verifico que no tenga incompatibilidades con las prendas de la listas
+    #     si tiene, creo su propia lista
+    #     si no, la agrego a una lista
+    
+# Escribo cada lista de lavado en archivo laundry file
+# Salgo ordenadamente
+
